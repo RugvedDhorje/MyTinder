cafe72e27bbf3f497128626cd5e1a797fe8ee190
Rugved Dhorje <rdhorje6@gmail.com>
2024-11-29 00:49:02 +0530

    added profile edit api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -75,5 +75,11 @@ authRouter.post("/login", async (req, res) => {
     res.status(400).send("Error: " + err.message);
   }
 });
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("Logout Successful"); 
+});
 
 module.exports = authRouter;
